8794c706114cdd07a98f583e83b83e406245ddc0
mprosser <Michael.Prosser@visitscotland.com>
2023-05-25 17:31:00 +0100

    feat(vue3): add global pinia object in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,18 @@
-/** @type { import('@storybook/vue3').Preview } */
+import { setup } from '@storybook/vue3';
+
+import { createPinia } from 'pinia';
+
+setup((app) => {
+  app.use(createPinia());
+});
 
 const preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-  },
+  decorators: [
+    (story) => ({
+      components: { story },
+      template: '<div style="margin: 3em;"><story /></div>',
+    }),
+  ],
 };
 
 export default preview;
