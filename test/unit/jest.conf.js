2c402391d6195a987ca214d011859f676f2f7f28
GemmaLouise <gemma_stephen@hotmail.co.uk>
2023-05-25 17:25:50 +0100

    feat(jest): implements functioning unit tests

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -1,35 +1,39 @@
-const path = require("path")
+const path = require('path');
 
 module.exports = {
-    rootDir: path.resolve(__dirname, "../../"),
-    roots: ["<rootDir>/src"],
-    testMatch: ["**/?(*.)jest.spec.js?(x)"],
-    modulePaths: ["<rootDir>"],
-    moduleFileExtensions: ["js", "json", "vue"],
+    rootDir: path.resolve(__dirname, '../../'),
+    roots: ['<rootDir>/src'],
+    testEnvironment: 'jsdom',
+    testEnvironmentOptions: {
+        customExportConditions: ['node', 'node-addons'],
+    },
+    testMatch: ['**/?(*.)jest.spec.js?(x)'],
+    modulePaths: ['<rootDir>'],
+    moduleFileExtensions: ['js', 'json', 'vue'],
     moduleNameMapper: {
-        "^@/(.*)$": "<rootDir>/src/$1",
-        "^@components/(.*)$": "<rootDir>/src/components/$1",
-        "^@test/(.*)$": "<rootDir>/test/$1",
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-            "<rootDir>/test/unit/mocks/fileMock.js",
-        "\\.svg$": "<rootDir>/test/unit/mocks/svgMock.js",
+        '^@components/(.*)$': '<rootDir>/src/components/$1',
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '^@test/(.*)$': '<rootDir>/test/$1',
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/test/unit/mocks/fileMock.js',
+        '\\.svg$': '<rootDir>/test/unit/mocks/svgMock.js',
     },
     transform: {
-        "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
-        ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest",
+        '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+        '.*\\.(vue)$': '<rootDir>/node_modules/@vue/vue3-jest',
     },
-    transformIgnorePatterns: ["<rootDir>/node_modules/(?!(bootstrap-vue)/)"],
-    snapshotSerializers: ["<rootDir>/node_modules/jest-serializer-vue"],
-    setupFiles: ["<rootDir>/test/unit/setup"],
-    coverageDirectory: "<rootDir>/test/unit/coverage",
+    transformIgnorePatterns: ['<rootDir>/node_modules/(?!(bootstrap-vue)/)'],
+    snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
+    setupFiles: ['<rootDir>/test/unit/setup'],
+    coverageDirectory: '<rootDir>/test/unit/coverage',
     collectCoverageFrom: [
-        "<rootDir>/src/**/*.{js,vue}",
-        "!<rootDir>/cypress/**/*.{js,vue}",
-        "!<rootDir>/**/*.spec.js",
-        "!<rootDir>/src/main.js",
-        "!<rootDir>/node_modules/**",
-        "!<rootDir>/src/system.js",
-        "!<rootDir>/src/system-components.js",
-        "!<rootDir>/docs/**/*",
+        '<rootDir>/src/**/*.{js,vue}',
+        '!<rootDir>/cypress/**/*.{js,vue}',
+        '!<rootDir>/**/*.spec.js',
+        '!<rootDir>/src/main.js',
+        '!<rootDir>/node_modules/**',
+        '!<rootDir>/src/system.js',
+        '!<rootDir>/src/system-components.js',
+        '!<rootDir>/docs/**/*',
     ],
-}
+};
