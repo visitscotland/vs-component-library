daf1a7d1b6d2ca07739bfbd03a4a950e7f6c2ce4
mprosser <Michael.Prosser@visitscotland.com>
2023-05-25 09:47:16 +0100

    feat(build): add comment to vue ts shim

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,3 +1,6 @@
+// This allows typescript files to consume vue SFCs properly and return the component.
+// In particular this is necessary in stories and index files.
+
 declare module '*.vue' {
     import { defineComponent } from 'vue';
     const component: ReturnType<typeof defineComponent>;
