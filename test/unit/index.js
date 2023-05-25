d0d3fcda577a25615c4fe597be172ce8845edc10
mprosser <Michael.Prosser@visitscotland.com>
2023-05-25 15:44:50 +0100

    feat(test): wIP jest setup

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,7 +1,3 @@
-import Vue from "vue"
-
-Vue.config.productionTip = false
-
 // require all test files (files that ends with .spec.js)
 const testsContext = require.context("../../src", true, /\.jest.spec$/)
 testsContext.keys().forEach(testsContext)
