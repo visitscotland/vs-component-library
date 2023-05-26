393c144c3e741e3d7d8cf0fb3bf27d3d676087da
GemmaLouise <gemma_stephen@hotmail.co.uk>
2023-05-26 16:21:54 +0100

    refactor(stories): fixes linting errors in stories

--- stories/elements/Heading.stories.js
+++ stories/elements/Heading.stories.js
@@ -5,17 +5,23 @@ export default {
     title: 'Elements/Heading',
 };
 
-const Template = args => ({
-    components: { VsHeading },
+const Template = (args) => ({
+    components: {
+        VsHeading,
+    },
     setup() {
-        return { args };
+        return {
+            args,
+        };
     },
     template: `
         <VsHeading v-bind="$props">
             <template v-if="${'default' in args}" v-slot>${args.default}</template>
-            <template v-if="${'sub-heading' in args}" v-slot:sub-heading>${args['sub-heading']}</template>
+            <template v-if="${'sub-heading' in args}" v-slot:sub-heading>${
+        args['sub-heading']
+    }</template>
         </VsHeading>
-    `
+    `,
 });
 
 const base = {
@@ -25,36 +31,41 @@ const base = {
     overrideStyleLevel: null,
     alternative: false,
     thin: false,
-}
-
-export const Default = Template.bind({});
-
-Default.args = {
-    ...base
 };
 
-export const OverrideStyleLevel = Template.bind({});
+export const Default = Template.bind({
+});
+
+Default.args = {
+    ...base,
+};
+
+export const OverrideStyleLevel = Template.bind({
+});
 
 OverrideStyleLevel.args = {
     ...base,
     overrideStyleLevel: 3,
 };
 
-export const WithSubheading = Template.bind({});
+export const WithSubheading = Template.bind({
+});
 
 WithSubheading.args = {
     ...base,
-    'sub-heading': 'test something'
+    'sub-heading': 'test something',
 };
 
-export const Thin = Template.bind({});
+export const Thin = Template.bind({
+});
 
 Thin.args = {
     ...base,
     thin: true,
 };
 
-export const Alternative = Template.bind({});
+export const Alternative = Template.bind({
+});
 
 Alternative.args = {
     ...base,
--- stories/elements/Image.stories.js
+++ stories/elements/Image.stories.js
@@ -1,20 +1,25 @@
 import VsImg from '@/components/elements/img/Img.vue';
 
 export default {
-  component: VsImg,
-  title: 'Elements/Img',
+    component: VsImg,
+    title: 'Elements/Img',
 };
 
-const Template = args => ({
-  components: { VsImg },
-  setup() {
-      return { args };
-  },
-  template: `
+const Template = (args) => ({
+    components: {
+        VsImg,
+    },
+    setup() {
+        return {
+            args,
+        };
+    },
+    template: `
     <VsImg
       src="src/assets/images/placeholders/list-default.jpg"
     />
   `,
 });
 
-export const Default = Template.bind({});
+export const Default = Template.bind({
+});
