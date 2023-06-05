2df7301187fc9d54019ec944db2319efc76ae11f
mprosser <Michael.Prosser@visitscotland.com>
2023-06-05 16:55:17 +0100

    feat(vue3): port tag to vue 3

---
+++ stories/elements/Tag.stories.js
@@ -0,0 +1,36 @@
+import VsTag from '@/components/elements/tag/Tag.vue';
+
+export default {
+    component: VsTag,
+    title: 'Elements/Tag',
+    tags: ['autodocs'],
+    argTypes: {
+
+    },
+};
+
+const Template = (args) => ({
+    components: {
+        VsTag,
+    },
+    setup() {
+        return {
+            args,
+        };
+    },
+    template: `
+        <VsTag v-bind="$props">
+            <template v-if="${'default' in args}" v-slot>${args.default}</template>
+        </VsTag>
+    `,
+});
+
+const base = {
+    default: 'Something',
+    href: '#',
+};
+
+export const Default = Template.bind({
+});
+
+Default.args = base;
