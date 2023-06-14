2a59697fbd949460fc25901f7ecd26b2b373f5db
mprosser <Michael.Prosser@visitscotland.com>
2023-06-14 16:50:30 +0100

    build(chromatic): add plugin to support turbosnap for vite

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -2,6 +2,7 @@
 
 const path = require("path");
 const { mergeConfig } = require('vite');
+const turbosnap = require('vite-plugin-turbosnap');
 
 const config = {
   stories: [
@@ -52,6 +53,7 @@ const config = {
           },
         ],
       },
+      plugins: configType === 'PRODUCTION' ? [turbosnap({ rootDir: config.root ?? process.cwd() })] : [],
     });
   },
 };
