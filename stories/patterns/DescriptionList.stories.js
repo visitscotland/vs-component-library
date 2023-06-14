58ce4f6373b1bf1ab27089c91502e9f1f397395d
mprosser <Michael.Prosser@visitscotland.com>
2023-06-14 12:51:10 +0100

    feat(vue3): port description list to vue 3

---
+++ stories/patterns/DescriptionList.stories.js
@@ -0,0 +1,46 @@
+import VsDescriptionList from '@/components/patterns/description-list/DescriptionList.vue';
+import VsDescriptionListItem from '@/components/patterns/description-list/components/DescriptionListItem.vue';
+
+export default {
+    component: VsDescriptionList,
+    title: 'Patterns/DescriptionList',
+    tags: ['autodocs'],
+};
+
+const Template = (args) => ({
+    components: {
+        VsDescriptionList,
+        VsDescriptionListItem,
+    },
+    setup() {
+        return {
+            args,
+        };
+    },
+    template: `
+        <VsDescriptionList v-bind="args">
+            <VsDescriptionListItem title>Highlights</VsDescriptionListItem>
+            <VsDescriptionListItem>East Neuk of Fife</VsDescriptionListItem>
+            <VsDescriptionListItem>St Andrews</VsDescriptionListItem>
+            <VsDescriptionListItem>Dundee</VsDescriptionListItem>
+            <VsDescriptionListItem>Angus</VsDescriptionListItem>
+        </VsDescriptionList>
+    `,
+});
+
+const base = {
+    inline: false,
+};
+
+export const Default = Template.bind({
+});
+
+Default.args = base;
+
+export const InLine = Template.bind({
+});
+
+InLine.args = {
+    ...base,
+    inline: true,
+};
