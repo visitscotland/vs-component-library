b7244bba0d32ecc3b165215d95b872d28d3410e1
mark.mitchell@visitscotland.com <mark.mitchell@visitscotland.com>
2023-06-19 12:19:10 +0100

    feat(skipto): remove deprecated syntax (.native modifier)

--- src/components/patterns/skip-to/SkipTo.vue
+++ src/components/patterns/skip-to/SkipTo.vue
@@ -1,48 +1,48 @@
 <template>
-  <nav
-      class="vs-skip-to d-flex"
-      data-test="vs-skip-to"
-      :aria-label="skipToText"
-  >
-      <p
-          class="vs-skip-to__label"
-          id="skip-to-label"
-      >
-          {{ skipToText }}:
-      </p>
-      <VsLink
-          href="#main-nav"
-          data-test="vs-skip-to-main-menu"
-          @click.native="mainMenuFocus"
-          id="main-menu"
-      >
-          <!-- @slot text for 'Main menu' -->
-          <slot name="mainMenuText" />
-      </VsLink>
-      <VsLink
-          href="#site-search-btn"
-          data-test="vs-skip-to-search"
-          class="vs-skip-to__search"
-      >
-          <!-- @slot text for 'Search' -->
-          <slot name="searchText" />
-      </VsLink>
-      <VsLink
-          href="#main-heading"
-          data-test="vs-skip-to-main-content"
-      >
-          <!-- @slot text for 'Main content' -->
-          <slot name="mainContentText" />
-      </VsLink>
-      <VsLink
-          href="#site-footer"
-          @click.native="footerFocus"
-          data-test="vs-skip-to-footer"
-      >
-          <!-- @slot text for 'Footer' -->
-          <slot name="footerText" />
-      </VsLink>
-  </nav>
+    <nav
+        class="vs-skip-to d-flex"
+        data-test="vs-skip-to"
+        :aria-label="skipToText"
+    >
+        <p
+            class="vs-skip-to__label"
+            id="skip-to-label"
+        >
+            {{ skipToText }}:
+        </p>
+        <VsLink
+            href="#main-nav"
+            data-test="vs-skip-to-main-menu"
+            @click="mainMenuFocus"
+            id="main-menu"
+        >
+            <!-- @slot text for 'Main menu' -->
+            <slot name="mainMenuText" />
+        </VsLink>
+        <VsLink
+            href="#site-search-btn"
+            data-test="vs-skip-to-search"
+            class="vs-skip-to__search"
+        >
+            <!-- @slot text for 'Search' -->
+            <slot name="searchText" />
+        </VsLink>
+        <VsLink
+            href="#main-heading"
+            data-test="vs-skip-to-main-content"
+        >
+            <!-- @slot text for 'Main content' -->
+            <slot name="mainContentText" />
+        </VsLink>
+        <VsLink
+            href="#site-footer"
+            @click="footerFocus"
+            data-test="vs-skip-to-footer"
+        >
+            <!-- @slot text for 'Footer' -->
+            <slot name="footerText" />
+        </VsLink>
+    </nav>
 </template>
 
 <script>
@@ -56,85 +56,85 @@ import VsLink from '@components/elements/link/Link';
 * @displayName Skip To
 */
 export default {
-  name: 'VsSkipTo',
-  status: 'prototype',
-  release: '0.0.1',
-  components: {
-      VsLink,
-  },
-  props: {
-      /**
+    name: 'VsSkipTo',
+    status: 'prototype',
+    release: '0.0.1',
+    components: {
+        VsLink,
+    },
+    props: {
+        /**
        * The skip to label for the menu
        */
-      skipToText: {
-          type: String,
-          required: true,
-      },
-  },
-  methods: {
-      /**
+        skipToText: {
+            type: String,
+            required: true,
+        },
+    },
+    methods: {
+        /**
        * Focuses on first item in main nav
       */
-      mainMenuFocus() {
-          const mobileMenuBtn = document.getElementsByClassName('vs-mega-nav__menu__mobile')[0];
-          const firstMenuItem = document.getElementsByClassName('vs-mega-nav-dropdown')[0];
+        mainMenuFocus() {
+            const mobileMenuBtn = document.getElementsByClassName('vs-mega-nav__menu__mobile')[0];
+            const firstMenuItem = document.getElementsByClassName('vs-mega-nav-dropdown')[0];
 
-          if (!this.isUndefined(firstMenuItem)) {
-              let firstMenuBtn = firstMenuItem.getElementsByClassName('btn')[0];
+            if (!this.isUndefined(firstMenuItem)) {
+                let firstMenuBtn = firstMenuItem.getElementsByClassName('btn')[0];
 
-              if (mobileMenuBtn.offsetParent !== null) {
-                  // if the mobile menu is visible, open it and focus
-                  // the first link
-                  mobileMenuBtn.querySelectorAll('.btn.dropdown-toggle')[0].click();
-                  const firstMobileMenuItem = document.getElementsByClassName('vs-mega-nav-accordion-item--level-1')[0];
-                  const firstMobileBtn = firstMobileMenuItem.querySelectorAll('.vs-button.vs-accordion-toggle')[0];
-                  firstMenuBtn = firstMobileBtn;
+                if (mobileMenuBtn.offsetParent !== null) {
+                    // if the mobile menu is visible, open it and focus
+                    // the first link
+                    mobileMenuBtn.querySelectorAll('.btn.dropdown-toggle')[0].click();
+                    const firstMobileMenuItem = document.getElementsByClassName('vs-mega-nav-accordion-item--level-1')[0];
+                    const firstMobileBtn = firstMobileMenuItem.querySelectorAll('.vs-button.vs-accordion-toggle')[0];
+                    firstMenuBtn = firstMobileBtn;
 
-                  // timeout need to ensure menu items to be accessible in DOM
-                  setTimeout(() => {
-                      firstMenuBtn.focus();
-                  }, 200);
-              } else {
-                  firstMenuBtn.focus();
-              }
-          }
-      },
-      /**
+                    // timeout need to ensure menu items to be accessible in DOM
+                    setTimeout(() => {
+                        firstMenuBtn.focus();
+                    }, 200);
+                } else {
+                    firstMenuBtn.focus();
+                }
+            }
+        },
+        /**
        * Focuses on first item footer nav
       */
-      footerFocus() {
-          const footerElement = document.getElementsByClassName('vs-footer')[0];
+        footerFocus() {
+            const footerElement = document.getElementsByClassName('vs-footer')[0];
 
-          if (!this.isUndefined(footerElement)) {
-              const firstFooterSection = footerElement.getElementsByClassName('vs-footer-accordion-item')[0];
-              const firstFooterLink = firstFooterSection.getElementsByClassName('vs-link')[0];
-              const footerMobileToggle = firstFooterSection.getElementsByClassName('vs-accordion-toggle')[0];
+            if (!this.isUndefined(footerElement)) {
+                const firstFooterSection = footerElement.getElementsByClassName('vs-footer-accordion-item')[0];
+                const firstFooterLink = firstFooterSection.getElementsByClassName('vs-link')[0];
+                const footerMobileToggle = firstFooterSection.getElementsByClassName('vs-accordion-toggle')[0];
 
-              // if mobile footer toggle link is visible click to open
-              if (footerMobileToggle.offsetParent !== null
-                  && !this.isUndefined(footerMobileToggle)) {
-                  footerMobileToggle.click();
-              }
+                // if mobile footer toggle link is visible click to open
+                if (footerMobileToggle.offsetParent !== null
+                    && !this.isUndefined(footerMobileToggle)) {
+                    footerMobileToggle.click();
+                }
 
-              if (!this.isUndefined(footerElement) && !this.isUndefined(firstFooterLink)) {
-                  footerElement.scrollIntoView(true);
+                if (!this.isUndefined(footerElement) && !this.isUndefined(firstFooterLink)) {
+                    footerElement.scrollIntoView(true);
 
-                  // focus the first footer link - timeout allows it to be
-                  // accessible in DOM for mobile accordion
-                  setTimeout(() => {
-                      firstFooterLink.focus();
-                  }, 200);
-              }
-          }
-      },
-      isUndefined(item) {
-          if (typeof item === 'undefined') {
-              return true;
-          }
+                    // focus the first footer link - timeout allows it to be
+                    // accessible in DOM for mobile accordion
+                    setTimeout(() => {
+                        firstFooterLink.focus();
+                    }, 200);
+                }
+            }
+        },
+        isUndefined(item) {
+            if (typeof item === 'undefined') {
+                return true;
+            }
 
-          return false;
-      },
-  },
+            return false;
+        },
+    },
 };
 </script>
 
