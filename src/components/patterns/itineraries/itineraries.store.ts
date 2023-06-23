833cdc356796f09d2d26daf7610dcf32baac2f0e
mprosser <Michael.Prosser@visitscotland.com>
2023-06-23 10:22:23 +0100

    fix(itineraries): correct highlighted stop type

--- src/components/patterns/itineraries/itineraries.store.ts
+++ src/components/patterns/itineraries/itineraries.store.ts
@@ -1,8 +1,26 @@
 import { ref, type Ref } from 'vue';
 import { defineStore } from 'pinia';
 
+type Geometry = {
+    type: string,
+    coordinates: number[],
+};
+
+type Properties = {
+    altText: string,
+    imageSrc: string,
+    stopCount: string,
+    title: string,
+};
+
+type Stop = {
+    type: string,
+    geometry: Geometry,
+    properties: Properties,
+};
+
 const useItinerariesStore = defineStore('itineraries', () => {
-    const highlightedStop: Ref<Object> = ref(null);
+    const highlightedStop: Ref<Stop> = ref(null);
 
     function setStopHighlighted(newStop) {
         highlightedStop.value = newStop;
