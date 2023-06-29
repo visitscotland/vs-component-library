68865c69ba0acea54ef8a1af3bfd5b09f2073c6e
mark.mitchell@visitscotland.com <mark.mitchell@visitscotland.com>
2023-06-29 14:37:53 +0100

    feat(patterns/summary-box): add story and controls

--- stories/patterns/SummaryBox.stories.js
+++ stories/patterns/SummaryBox.stories.js
@@ -1,63 +1,144 @@
-import  VsSummaryBoxList from '@/components/patterns/summary-box/SummaryBoxList.vue';
-import  VsSummaryBoxListItem from '@/components/patterns/summary-box/components/SummaryBoxListItem.vue';
+import VsSummaryBoxList from '@/components/patterns/summary-box/SummaryBoxList.vue';
+import VsSummaryBoxListItem from '@/components/patterns/summary-box/components/SummaryBoxListItem.vue';
+import VsSummaryBoxDistanceListItem from '@/components/patterns/summary-box/components/SummaryBoxDistanceListItem.vue';
+
+import sampleItinerary from '@/assets/fixtures/itineraries/sample-itinerary.json';
 
 export default {
     component: VsSummaryBoxList,
     title: 'Patterns/Summary Box',
     tags: ['autodocs'],
-    argTypes: {},
+    argTypes: {
+        totalDays: {
+            control: {
+                type: 'number',
+            },
+        },
+        totalMiles: {
+            control: {
+                type: 'number',
+            },
+        },
+        totalKM: {
+            control: {
+                type: 'number',
+            },
+        },
+        transportKey: {
+            options: [
+                'none - icon is optional',
+                'car',
+                'bus',
+                'cycle',
+                'train',
+                'ferry',
+                'walk',
+                'tram',
+                'taxi',
+                'subway',
+                'public-transport',
+                'boat',
+            ],
+            control: {
+                type: 'select',
+            },
+        },
+        transportValue: {
+            control: {
+                type: 'string',
+            },
+        },
+        themeKey: {
+            options: [
+                'none - icon is optional',
+                'child',
+                'family',
+                'film-tv',
+                'city',
+                'camera',
+                'castle',
+                'food',
+                'hiking',
+                'history',
+                'leaf',
+                'ski-boot',
+                'whisky',
+            ],
+            control: {
+                type: 'select',
+            },
+        },
+        themeValue: {
+            options: [
+                'Families',
+                'Film & TV',
+                'City Break',
+                'Photography',
+                'Architecture',
+                'Food',
+                'Hiking',
+                'History',
+                'Nature',
+                'Skiing',
+                'Whisky',
+            ],
+            control: {
+                type: 'select',
+            },
+        },
+    },
 };
 
-const Template = () => ({
+const Template = (args) => ({
     components: {
         VsSummaryBoxList,
         VsSummaryBoxListItem,
+        VsSummaryBoxDistanceListItem,
     },
-    // setup() {
-    //     return {
-    //         args,
-    //     };
-    // },
+    setup() {
+        return {
+            args,
+        };
+    },
     template: `
-        <div style="height: 400px;">
             <VsSummaryBoxList>
                 <VsSummaryBoxListItem
-                    :text=itineraries.sampleItinerary.totalDays
+                    :text='args.totalDays'
                     label="Days"
                 />
                 <VsSummaryBoxDistanceListItem
-                    :miles=itineraries.sampleItinerary.totalMiles
-                    :kilometres=itineraries.sampleItinerary.totalKM
+                    :miles='args.totalMiles'
+                    :kilometres='args.totalKM'
                     distance-label="Distance"
                     miles-label="miles"
                     kilometres-label="km"
                 >
                 </VsSummaryBoxDistanceListItem>
                 <VsSummaryBoxListItem
-                    :icon=itineraries.sampleItinerary.transport.key
-                    :iconLabel=itineraries.sampleItinerary.transport.value
+                    :icon='args.transportKey'
+                    :iconLabel='args.transportValue'
                     label="Transport"
                 />
                 <VsSummaryBoxListItem
-                    :icon=itineraries.sampleItinerary.theme.key
-                    :iconLabel=itineraries.sampleItinerary.theme.value
+                    :icon='args.themeKey'
+                    :iconLabel='args.themeValue'
                     label="Main theme"
                 />
             </VsSummaryBoxList>
-        </div>
     `,
 });
 
-// const base = {
-//     linkCount: 4,
-//     label: 'Visit Scotland',
-//     type: 'external',
-//     href: 'https://visitscotland.com',
-//     variant: 'primary',
-// };
+const base = {
+    totalDays: sampleItinerary.totalDays,
+    totalMiles: sampleItinerary.totalMiles,
+    totalKM: sampleItinerary.totalKM,
+    transportKey: sampleItinerary.transport.key,
+    transportValue: sampleItinerary.transport.value,
+    themeKey: sampleItinerary.theme.key,
+    themeValue: sampleItinerary.theme.value,
+};
 
 export const Default = Template.bind({
 });
 
-// Default.args = base;
-
+Default.args = base;
