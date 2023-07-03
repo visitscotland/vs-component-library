12fc8253afe7a11009022a43f02b6dadb4724468
mprosser <Michael.Prosser@visitscotland.com>
2023-07-03 15:33:05 +0100

    feat(icentre): add missing quote and image with caption to icentre stories

--- stories/patterns/Icentre.stories.js
+++ stories/patterns/Icentre.stories.js
@@ -1,4 +1,9 @@
 import VsIcentre from '@/components/patterns/icentre/Icentre.vue';
+import VsImageWithCaption from '@/components/patterns/image-with-caption/ImageWithCaption.vue';
+import VsQuote from '@/components/patterns/quote/Quote.vue';
+import VsCaption from '@/components/patterns/caption/Caption.vue';
+import VsButton from '@/components/elements/button/Button.vue';
+import VsImg from '@/components/elements/img/Img.vue';
 
 export default {
     component: VsIcentre,
@@ -9,6 +14,11 @@ export default {
 const Template = (args) => ({
     components: {
         VsIcentre,
+        VsImageWithCaption,
+        VsQuote,
+        VsCaption,
+        VsButton,
+        VsImg,
     },
     setup() {
         return {
@@ -17,25 +27,84 @@ const Template = (args) => ({
     },
     template: `
         <VsIcentre>
+            <template v-slot:icentre-image-with-caption>
+                <VsImageWithCaption
+                    :imageSrc="args.imageSrc"
+                    :altText="args.imageAltText"
+                >
+                    <template v-slot:img-caption>
+                        <VsCaption>
+                            <template v-slot:caption>
+                                <span>{{ args.imageCaption }}</span>
+                            </template>
+
+                            <template v-slot:credit>
+                                <span>{{ args.imageCredit }}</span>
+                            </template>
+                        </VsCaption>
+                    </template>
+                </VsImageWithCaption>
+            </template>
+
             <template v-slot:icentre-quote>
-                Add Quote Here
+                <VsQuote variant="wide">
+                    <template v-slot:quote-image>
+                        <VsImg
+                            :src="args.quoteImageSrc"
+                            :alt="args.quoteImageAltText"
+                        >
+                        </VsImg>
+                    </template>
+                    <template v-slot:quote-content>
+                        {{ args.quoteContent }}
+                    </template>
+                    <template v-slot:quote-author-name>
+                        {{ args.quoteAuthor }}
+                    </template>
+                    <template v-slot:quote-author-title>
+                        {{ args.quoteAuthorTitle }}
+                    </template>
+                    <template v-slot:quote-link>
+                        <VsButton
+                            :args="buttonHref"
+                        >
+                            {{ args.buttonText }}
+                        </VsButton>
+                </template>
+                </VsQuote>
             </template>
 
             <template v-slot:icentre-links>
                 <p>
-                    Test content <a href="#">link</a>
+                    {{ args['icentre-links'] }}
                 </p>
             </template>
         </VsIcentre>
     `,
 });
 
-// Missing both quote and ImagewithCaption while those are wip in another branch
-
 const base = {
+    imageSrc: '/fixtures/icentre/icentre.jpg',
+    imageAltText: 'Aberdeen iCentre',
+    imageCaption: 'VisitScotland Information Centre',
+    imageCredit: '&copy; VisitScotland',
+    quoteImageSrc: '/fixtures/icentre/icentre.jpg',
+    quoteImageAltText: 'Penny, icentre staff',
+    quoteContent: 'Look out for the incredible architecture, parks and gardens as you browse independent shops, galleries and eateries.',
+    quoteAuthor: 'Penny',
+    quoteAuthorTitle: 'Visitor Services Advisor at Aberdeen iCentre',
+    buttonHref: '#',
+    buttonText: 'See more',
+    'icentre-links': 'See all Aberdeen attractions',
 };
 
 export const Default = Template.bind({
 });
 
 Default.args = base;
+
+Default.parameters = {
+    backgrounds: {
+        default: 'Grey',
+    },
+};
