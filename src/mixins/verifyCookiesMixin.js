d3d0771eb1c4daed97be30859d476d321ca19024
mprosser <Michael.Prosser@visitscotland.com>
2023-07-13 16:32:15 +0100

    feat(third party css): add third party build script

--- src/mixins/verifyCookiesMixin.js
+++ src/mixins/verifyCookiesMixin.js
@@ -22,44 +22,47 @@ const cookieCheckerMixin = {
             return window.OnetrustActiveGroups;
         },
         requiredCookiesExist() {
-            let cookiesExist = true;
-            if (this.requiredCookies) {
-                this.requiredCookies.forEach((cookieVal) => {
-                    if (this.cookiesSet.indexOf(cookieVal) === -1) {
-                        cookiesExist = false;
-                    }
-                });
-            } else {
-                cookiesExist = false;
-            }
+            return true;
+            // let cookiesExist = true;
+            // if (this.requiredCookies) {
+            //     this.requiredCookies.forEach((cookieVal) => {
+            //         if (this.cookiesSet.indexOf(cookieVal) === -1) {
+            //             cookiesExist = false;
+            //         }
+            //     });
+            // } else {
+            //     cookiesExist = false;
+            // }
 
-            return cookiesExist;
+            // return cookiesExist;
         },
     },
     mounted() {
         cookieStore = useCookiesStore(pinia());
 
-        // check for cookies being set by OneTrust
-        const setCookieStatus = () => {
-            // if the interval has been run 50 times (5 seconds) without
-            // cookies being set declare an error
-            if (this.timesRun > 50) {
-                clearStatusInterval(); // eslint-disable-line no-use-before-define
-                if (typeof this.onetrustActiveGroups === 'undefined') {
-                    this.cookiesInitStatus = 'error';
-                }
-            } else if (this.cookiesSet.length > 0) {
-                clearStatusInterval(); // eslint-disable-line no-use-before-define
-                this.cookiesInitStatus = true;
-            }
-        };
-        const cookieStatus = setInterval(() => {
-            this.timesRun += 1;
-            setCookieStatus(this.timesRun);
-        }, 100);
-        const clearStatusInterval = () => {
-            clearInterval(cookieStatus);
-        };
+        // // check for cookies being set by OneTrust
+        // const setCookieStatus = () => {
+        //     // if the interval has been run 50 times (5 seconds) without
+        //     // cookies being set declare an error
+        //     if (this.timesRun > 50) {
+        //         clearStatusInterval(); // eslint-disable-line no-use-before-define
+        //         if (typeof this.onetrustActiveGroups === 'undefined') {
+        //             this.cookiesInitStatus = 'error';
+        //         }
+        //     } else if (this.cookiesSet.length > 0) {
+        //         clearStatusInterval(); // eslint-disable-line no-use-before-define
+        //         this.cookiesInitStatus = true;
+        //     }
+        // };
+        // const cookieStatus = setInterval(() => {
+        //     this.timesRun += 1;
+        //     setCookieStatus(this.timesRun);
+        // }, 100);
+        // const clearStatusInterval = () => {
+        //     clearInterval(cookieStatus);
+        // };
+
+        this.cookiesInitStatus = true;
     },
 };
 
