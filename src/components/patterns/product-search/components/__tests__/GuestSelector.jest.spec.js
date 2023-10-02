ded79410800ec9a9124a067c1066edd6ecd678bf
GemmaLouise <gemma_stephen@hotmail.co.uk>
2023-10-02 12:39:58 +0100

    test(psw): add unit tests for guest selection

--- src/components/patterns/product-search/components/__tests__/GuestSelector.jest.spec.js
+++ src/components/patterns/product-search/components/__tests__/GuestSelector.jest.spec.js
@@ -14,27 +14,53 @@ const factoryShallowMount = (propsData) => shallowMount(VsGuestSelector, {
 describe('VsGuestSelector', () => {
     it('should render a component with the data-test attribute `vs-guest-selector`', () => {
         const wrapper = factoryShallowMount();
-        console.log(wrapper.html())
         expect(wrapper.attributes('data-test')).toBe('vs-guest-selector');
     });
 
-    // describe(':props', () => {
-    //     it('renders the label with correct `ID` and `label` text', async () => {
-    //         const wrapper = factoryShallowMount();
-    //         const label = wrapper.find('label');
+    describe(':props', () => {
+        it('disables the trigger button if `availabilty` is false', async () => {
+            const wrapper = factoryShallowMount({
+                availability: false,
+            });
+            const trigger = wrapper.find('.vs-guest-selector__trigger');
 
-    //         expect(label.attributes('for')).toBe('startDate');
-    //         expect(label.text()).toContain('Start Date');
-    //     });
-    // });
+            expect(trigger.attributes('disabled')).toBe("");
+        });
 
-    // describe(':events', () => {
-    //     it('should emit a `changeDate` event when date value is changed', async() => {
-    //         const wrapper = factoryShallowMount();
-    //         const input = wrapper.find('input[type="date"]');
-    //         await input.setValue('2023-09-26')
+        it('does not disable the trigger if `availabilty` is true', async () => {
+            const wrapper = factoryShallowMount();
+            const trigger = wrapper.find('.vs-guest-selector__trigger');
 
-    //         expect(wrapper.emitted('changeDate')[0][0]).toBe('2023-09-26');
-    //     });
-    // });
+            expect(trigger.attributes('disabled')).toBe(undefined);
+        });
+    });
+
+    describe(':interactions', () => {
+        it('should open the popover when button is clicked', async() => {
+            const wrapper = factoryShallowMount();
+            const trigger = wrapper.find('.vs-guest-selector__trigger');
+            const modal = wrapper.find('.vs-guest-selector__modal');
+            
+            expect(modal.classes()).not.toContain('vs-guest-selector__modal--visible');
+            
+            await trigger.trigger('click');
+
+            expect(modal.classes()).toContain('vs-guest-selector__modal--visible');
+        });
+
+        it('should close the popover when close button is clicked', async() => {
+            const wrapper = factoryShallowMount();
+            const trigger = wrapper.find('.vs-guest-selector__trigger');
+            const closeBtn = wrapper.find('[data-test="vs-guest-selector__close-btn"]');
+            const modal = wrapper.find('.vs-guest-selector__modal');
+
+            await trigger.trigger('click');
+
+            expect(modal.classes()).toContain('vs-guest-selector__modal--visible');
+            
+            await closeBtn.trigger('click');
+
+            expect(modal.classes()).not.toContain('vs-guest-selector__modal--visible');
+        });
+    });
 });
---
+++ src/components/patterns/product-search/components/__tests__/GuestSelectorNumberGroup.jest.spec.js
@@ -0,0 +1,30 @@
+import { shallowMount, config } from '@vue/test-utils';
+import VsGuestSelectorNumberGroup from '../GuestSelectorNumberGroup.vue';
+
+config.global.renderStubDefaultSlot = true;
+
+const factoryShallowMount = () => shallowMount(VsGuestSelectorNumberGroup, {
+    propsData: {
+        adultCount: 2,
+        childrenCount: 0,
+        count: 2,
+        disabled: false,
+        id: '17ec8af2-a0c9-4fa4-87d5-0b2a62390ab0',
+        infantCount: 0,
+        inputType: {'minCount':1,'maxCount':30,'count':2,'name':'r1a','unitLabel':'Adults'},
+        maxValue: 30,
+        minValue: 1,
+        name: 'r1a',
+        rowName: 'Adults',
+        rowLabel: 'r1',
+        unitRemoved: false,
+    }
+});
+
+
+describe('VsGuestSelectorNumberGroup', () => {
+    it('should render a component with the data-test attribute `vs-guest-selector`', () => {
+        const wrapper = factoryShallowMount();
+        expect(wrapper.attributes('data-test')).toBe('vs-guest-selector-number-group');
+    });
+});
