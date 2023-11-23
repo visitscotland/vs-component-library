c099eae2be0058f6b78a8a32890b1e0545a17fdb
mprosser <Michael.Prosser@visitscotland.com>
2023-11-23 09:12:45 +0000

    feat(images): reduce code duplication in src set mixin

--- src/mixins/srcSetMixin.js
+++ src/mixins/srcSetMixin.js
@@ -1,20 +1,11 @@
 const srcSetMixin = {
     computed: {
         fullSrcSet() {
-            if (!this.imgSrc) {
-                return '';
-            }
-
-            let connector = '?';
-
-            if (this.imgSrc.includes('?')) {
-                connector = '&';
-            }
-
-            return `${this.imgSrc}${connector}size=xs 300w,
-                ${this.imgSrc}${connector}size=sm 600w,
-                ${this.imgSrc}${connector}size=md 1200w,
-                ${this.imgSrc}${connector}size=lg 2048w`;
+            return `
+                ${this.specificImgSize('xs')} 300w,
+                ${this.specificImgSize('sm')} 600w,
+                ${this.specificImgSize('md')} 1200w,
+                ${this.specificImgSize('lg')} 2048w`;
         },
     },
     methods: {
