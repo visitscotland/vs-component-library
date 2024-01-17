e1b15cee86d783062b4b25e01ab7555fbc47ae19
Duncan Sanderson <duncan.sanderson@visitscotland.com>
2024-01-17 12:02:35 +0000

    fix(vite.config.js): define process
    process was causing the build process to fail on the consumer app

--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,56 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import ViteRequireContext from '@originjs/vite-plugin-require-context';
 
-export default defineConfig({
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: '@import "@/styles/core.styles.scss";',
-            },
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        define: {
+            'process.env': JSON.stringify(env),
+            'process.versions': process.versions,
+            'process.platform': `"${process.platform}"`,
         },
-    },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-            find: '@vue/runtime-core',
-            replacement: '@vue/runtime-core/dist/runtime-core.esm-bundler.js',
-        },
-    },
-    plugins: [
-        vue(),
-        ViteRequireContext(),
-    ],
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'src/test.js'),
-            name: 'vs-component-library',
-            fileName: 'vs-component-library',
-        },
-        rollupOptions: {
-            external: [
-                'vue',
-                'bootstrap-vue-next',
-                'youtube-vue3',
-            ],
-            output: {
-                globals: {
-                    vue: 'Vue',
-                    'bootstrap-vue-next': 'BootstrapVueNext',
-                    'youtube-vue3': 'YoutubeVue3',
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@import "@/styles/core.styles.scss";',
                 },
             },
         },
-    },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+                find: '@vue/runtime-core',
+                replacement: '@vue/runtime-core/dist/runtime-core.esm-bundler.js',
+            },
+        },
+        plugins: [
+            vue(),
+            ViteRequireContext(),
+        ],
+        build: {
+            lib: {
+                entry: path.resolve(__dirname, 'src/test.ts'),
+                name: 'vs-component-library',
+                fileName: 'vs-component-library',
+            },
+            rollupOptions: {
+                external: [
+                    'vue',
+                    'bootstrap-vue-next',
+                    'youtube-vue3',
+                ],
+                output: {
+                    dir: 'dist/components',
+                    globals: {
+                        vue: 'Vue',
+                        'bootstrap-vue-next': 'BootstrapVueNext',
+                        'youtube-vue3': 'YoutubeVue3',
+                    },
+                },
+            },
+        },
+    };
 });
