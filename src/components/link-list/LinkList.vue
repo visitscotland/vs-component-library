7477e3acecdd275575d6ba5086c79b9c833f8e6b
Duncan Sanderson <duncan.sanderson@visitscotland.com>
2024-08-09 15:20:18 +0100

    feat(linklist): add table of contents variant

--- src/components/link-list/LinkList.vue
+++ src/components/link-list/LinkList.vue
@@ -1,10 +1,19 @@
 <template>
-    <nav>
-        <ul class="link-list">
-            <!-- @slot Slot for link list items -->
-            <slot />
-        </ul>
-    </nav>
+    <div :class="classes">
+        <VsHeading
+            v-if="hasHeadingSlot"
+            level="2"
+            overrideStyleLevel="3"
+        >
+            <slot name="heading" />
+        </VsHeading>
+        <nav>
+            <ul class="link-list__list">
+                <!-- @slot Slot for link list items -->
+                <slot />
+            </ul>
+        </nav>
+    </div>
 </template>
 
 <script>
@@ -14,17 +23,46 @@
  * @displayName Link List
  */
 
+import VsHeading from '@/components/heading/Heading.vue';
+
 export default {
     name: 'VsLinkList',
     status: 'prototype',
     release: '0.0.1',
+    components: {
+        VsHeading,
+    },
+    props: {
+        toc: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    computed: {
+        classes() {
+            return {
+                'link-list': true,
+                'link-list--toc': this.toc,
+            };
+        },
+        hasHeadingSlot() {
+            return !!this.$slots.heading;
+        },
+    },
 };
 </script>
 
 <style lang="scss">
     .link-list {
-        list-style: none;
-        padding: 0;
-        margin-bottom: $spacer-7;
+        &__list {
+            list-style: none;
+            padding: 0;
+            margin-bottom: $spacer-7;
+        }
+
+        &--toc {
+            background: lightgray;
+            padding: 1rem 2rem;
+        }
     }
 </style>
