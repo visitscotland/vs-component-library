7bbb861b51a758c2c6edd8dcfd62a6aea0dae51d
Duncan Sanderson <duncan.sanderson@visitscotland.com>
2024-08-09 15:27:29 +0100

    feat(linklist.stories.js): add table of contents variant

--- stories/LinkList.stories.js
+++ stories/LinkList.stories.js
@@ -18,23 +18,6 @@ export default {
     title: 'LinkList',
     tags: ['autodocs'],
     argTypes: {
-        type: {
-            options: [
-                'default',
-                'external',
-                'internal',
-                'download',
-                'video',
-            ],
-            control: 'radio',
-        },
-        variant: {
-            options: [
-                'primary',
-                'on-dark',
-            ],
-            control: 'inline-radio',
-        },
     },
 };
 
@@ -49,7 +32,14 @@ const Template = (args) => ({
         };
     },
     template: `
-        <VsLinkList>
+        <VsLinkList v-bind="args">
+            <template
+                v-if="args.heading"
+                v-slot:heading
+            >
+                {{ args.heading }}
+            </template>
+
             <VsLinkListItem
                 v-for="link in args.linkList"
                 :type="link.type"
@@ -65,21 +55,22 @@ const Template = (args) => ({
             modalId="g-Fhvj7vW-E"
             closeBtnText="Close"
             :isVideoModal="true"
+            v-if="args.linkList.find((el) => el.type === 'video')"
         >
             <div
                 :class="args.jsDisabled ? 'no-js' : ''"
             >
-            <VsRow>
-                <VsCol cols="12">
-                    <VsVideo
-                        videoId="g-Fhvj7vW-E"
-                        class="mb-8"
-                        cookie-btn-text="Manage cookies"
-                        no-js-message="You need Javascript enabled to see this video"
-                        no-cookies-message="You need cookies enabled to see this video"
-                    />
-                </VsCol>
-            </VsRow>
+                <VsRow>
+                    <VsCol cols="12">
+                        <VsVideo
+                            videoId="g-Fhvj7vW-E"
+                            class="mb-8"
+                            cookie-btn-text="Manage cookies"
+                            no-js-message="You need Javascript enabled to see this video"
+                            no-cookies-message="You need cookies enabled to see this video"
+                        />
+                    </VsCol>
+                </VsRow>
             </div>
         </VsModal>
     `,
@@ -112,27 +103,44 @@ export const WithVideo = Template.bind({
 });
 
 WithVideo.args = {
-    ...base,
     linkList: [
-        {
-            title: 'Ski Accommodation',
-            href: '#',
-        },
-        {
-            title: '13 Amazing Places to Stay on Scotland\'s UNESCO Trail',
-            href: '#',
-        },
-        {
-            title: 'Stay in a converted church in Scotland',
-            type: 'external',
-            href: '#',
-        },
+        ...base.linkList,
         {
             title: 'Dark Skies in Scotland',
             type: 'video',
             href: '#',
             videoId: 'g-Fhvj7vW-E',
             videoDescriptor: 'Play',
+        },
+    ],
+};
+
+export const TableOfContents = Template.bind({
+});
+
+TableOfContents.args = {
+    heading: 'In this article:',
+    toc: true,
+    linkList: [
+        {
+            title: 'Who needs a short-term lets licence?',
+            href: '#section1',
+        },
+        {
+            title: 'When do I need to have a licence?',
+            href: '#section2',
+        },
+        {
+            title: 'How to apply for a licence',
+            href: '#section3',
+        },
+        {
+            title: 'Frequently Asked Questions (FAQ)',
+            href: '#section4',
+        },
+        {
+            title: 'Industry Advisory Group (IAG)',
+            href: '#section5',
         },
     ],
 };
