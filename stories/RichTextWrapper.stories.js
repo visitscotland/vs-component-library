4e75d6e60658009e7142b5af5c4cf1ed4cfc89fd
GemmaLouise <gemma_stephen@hotmail.co.uk>
2024-10-10 11:44:00 +0100

    refactor(heading): updating headings to use headingStyle prop

--- stories/RichTextWrapper.stories.js
+++ stories/RichTextWrapper.stories.js
@@ -42,13 +42,13 @@ const Template = (args) => ({
 
 const base = {
     default: `
-        <VsHeading level="3">
+        <VsHeading level="3" heading-style="heading-m">
             Experiencing Ben Nevis
         </VsHeading>
         <p>Ben Nevis is the king of them all. In the north west <a href='#'>Highlands</a>, near the town of <a href='#'>Fort William</a> and part of the <a href='#'>Grampian Mountain</a> range, the famous peak attracts 125k walkers a year. Whether you're an avid ambler or you just love beautiful landscapes, bagging 'the Ben' is likely to feature near the top of your Scottish bucket list.</p>
         <p>An ancient giant of the land, Ben Nevis was once a massive active volcano which exploded and collapsed inwards on itself millions of years ago. At the summit, there is evidence of an explosion in the form of light-coloured granite. The name itself has two translations from the ancient Gaelic language, meaning 'mountain with its head in the clouds', thanks to its iconic mist-shrouded peak, or it can also mean 'venomous mountain' - you can decide which translation you prefer after the climb!</p>
         
-        <VsHeading level="4">
+        <VsHeading level="4" heading-style="heading-s">
             More Information
         </VsHeading>
         <p>Read on for an overview of walking routes up the mountain, or visit <a href='#'>Walk Highlands</a> for detailed maps, difficulty levels and walking advice.</p>
