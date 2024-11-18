2c20fc073cd00f63e7653f1885bf343bb7a5efe3
Duncan Sanderson <duncan.sanderson@visitscotland.com>
2024-11-18 14:58:04 +0000

    feat(vs badge): add new badge component

---
+++ src/components/badge/VsBadge.vue
@@ -0,0 +1,27 @@
+<template>
+    <div
+        class="vs-badge"
+        data-test="vs-badge"
+    >
+        <BBadge>
+            <!-- @slot The badge content goes here -->
+            <slot />
+        </BBadge>
+    </div>
+</template>
+
+<script setup>
+import { BBadge } from 'bootstrap-vue-next';
+</script>
+
+<style lang="scss">
+.vs-badge {
+    border: 1px solid $vs-color-border-input;
+    border-radius: $border_radius_default;
+    color: $vs-color-text-less-subtle;
+    display: inline-block;
+    font-size: $font-size-3;
+    margin-right: $spacer-050;
+    padding: $spacer-025 $spacer-050;
+}
+</style>
---
+++ stories/Badge.stories.js
@@ -0,0 +1,33 @@
+import VsBadge from '@/components/badge/VsBadge.vue';
+
+export default {
+    component: VsBadge,
+    title: 'Badge',
+    tags: ['autodocs'],
+};
+const Template = (args) => ({
+    components: {
+        VsBadge,
+    },
+    setup() {
+        return {
+            args,
+        };
+    },
+    template: `
+        <VsBadge v-for="badge in args.badges">
+            {{ badge }}
+        </VsBadge>
+    `,
+});
+
+const base = {
+    badges: [
+        'Read time',
+        'Content type',
+    ],
+};
+
+export const Default = Template.bind();
+
+Default.args = base;
