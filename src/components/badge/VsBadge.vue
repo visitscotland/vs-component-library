e97053551fff1f6365b41d9bf31481e87967c71a
Duncan Sanderson <duncan.sanderson@visitscotland.com>
2024-11-20 16:29:02 +0000

    feat(badge): update colour tokens

--- src/components/badge/VsBadge.vue
+++ src/components/badge/VsBadge.vue
@@ -16,9 +16,9 @@ import { BBadge } from 'bootstrap-vue-next';
 
 <style lang="scss">
 .vs-badge {
-    border: 1px solid $vs-color-border-input;
+    border: 1px solid $vs-color-new-border-secondary;
     border-radius: $border_radius_default;
-    color: $vs-color-text-less-subtle;
+    color: $vs-color-new-text-secondary;
     display: inline-block;
     font-size: $font-size-3;
     margin-right: $spacer-050;
