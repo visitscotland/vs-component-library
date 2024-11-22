e4439dbd51781321119dfc8f5a2be8b11cb24c08
GemmaLouise <gemma_stephen@hotmail.co.uk>
2024-11-22 14:27:34 +0000

    docs(icon): update icon stories to include variants of icon component

--- stories/Icon.stories.js
+++ stories/Icon.stories.js
@@ -42,29 +42,125 @@ const Template = (args) => ({
         };
     },
     template: `
-        <VsIcon v-bind="args"></VsIcon>
+        <VsIcon 
+            class="me-125"
+            v-for="icon in args.icons"
+            :name="icon.name"
+            :variant="icon.variant"
+            :size="icon.size"
+            :customColour="icon.customColour"
+            :duotone="icon.duotone"
+            :style="icon.style"
+        ></VsIcon>
     `,
 });
-
-const base = {
-    name: 'user',
-    variant: 'primary',
-    size: 'lg',
-};
 
 export const Default = Template.bind({
 });
 
 Default.args = {
-    ...base,
+    icons: [
+        {
+            name: 'user',
+        },
+    ],
+};
+
+export const Size = Template.bind({
+});
+
+Size.args = {
+    icons: [
+        {
+            name: 'user',
+            size: 'xxs',
+        },
+        {
+            name: 'user',
+            size: 'xs',
+        },
+        {
+            name: 'user',
+            size: 'sm',
+        },
+        {
+            name: 'user',
+            size: 'md',
+        },
+        {
+            name: 'user',
+            size: 'lg',
+        },
+        {
+            name: 'user',
+            size: 'xl',
+        },
+    ],
+};
+
+export const Colour = Template.bind({
+});
+
+Colour.args = {
+    icons: [
+        {
+            name: 'user',
+            variant: 'default',
+        },
+        {
+            name: 'user',
+            variant: 'primary',
+        },
+        {
+            name: 'user',
+            variant: 'inverse',
+        },
+        {
+            name: 'user',
+            variant: 'tertiary',
+        },
+        {
+            name: 'user',
+            variant: 'danger',
+        },
+        {
+            name: 'user',
+            variant: 'warning',
+        },
+        {
+            name: 'user',
+            variant: 'disabled',
+        },
+    ],
+};
+
+Colour.parameters = {
+    backgrounds: {
+        default: 'Grey',
+    },
+};
+
+export const CustomColour = Template.bind({
+});
+
+CustomColour.args = {
+    icons: [
+        {
+            name: 'user',
+            customColour: '#55ACEE',
+        },
+    ],
 };
 
 export const Duotone = Template.bind({
 });
 
 Duotone.args = {
-    ...base,
-    name: 'crow',
-    duotone: true,
-    style: '--fa-secondary-opacity: 1.0; --fa-primary-color: dodgerblue; --fa-secondary-color: gold;',
+    icons: [
+        {
+            name: 'crow',
+            duotone: true,
+            style: '--fa-secondary-opacity: 1.0; --fa-primary-color: blue; --fa-secondary-color: gold;',
+        },
+    ],
 };
