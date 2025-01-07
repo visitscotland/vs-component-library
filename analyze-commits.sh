c8b895c957c1cdda970a442178ea165b66a0c419
mprosser <Michael.Prosser@visitscotland.com>
2025-01-07 15:29:30 +0000

    ci(semantic release): enforce 0 status

--- analyze-commits.sh
+++ analyze-commits.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -n "$1" ] && [ "$1" == "releases" ]; then
-    echo "$1" >&2
+    echo "$1" 1>&2
     exit 0
 else
-    echo "$@" >&2
-    exit 1
+    echo "$@" 1>&2
+    exit 0
 fi
