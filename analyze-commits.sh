90b33ae9c851377ecc0124603bffc2c47601fddf
mprosser <Michael.Prosser@visitscotland.com>
2025-01-07 16:12:27 +0000

    ci(semantic release): restore failure to test

--- analyze-commits.sh
+++ analyze-commits.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+for i in $@;do
+    echo $i 1>&2
+done;
+
 if [ -n "$1" ] && [ "$1" == "releases" ]; then
     echo "$1" 1>&2
     exit 0
 else
     echo "$@" 1>&2
-    exit 0
+    exit 1
 fi
