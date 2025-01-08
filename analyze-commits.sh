03b09ade53f05be274aa73f2033a62a8596e5e49
mprosser <Michael.Prosser@visitscotland.com>
2025-01-08 08:59:39 +0000

    ci(semantic release): enforce failure, debug log objects

--- analyze-commits.sh
+++ analyze-commits.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-echo "$1" 1>&2
+echo "commits" 1>&2
 
-echo "$2" 1>&2
+for i in "${$1[@]}"; do
+    echo "$i" 1>&2
+done
+
+echo "releases" 1>&2
+
+for i in "${$2[@]}"; do
+    echo "$i" 1>&2
+done
+
+echo "lastRelease" 1>&2
 
 echo "$3" 1>&2
 
 if [ -n "$1" ]; then
+    echo "A release is happening" 1>&2
     echo "$1" 1>&2
-    exit 0
+    exit 1
 else
+    echo "No release, killing CI" 1>&2
     exit 1
 fi
