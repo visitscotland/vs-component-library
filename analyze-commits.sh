7f3ca81812507300138d5af888fdf6e348692e8d
mprosser <Michael.Prosser@visitscotland.com>
2025-01-08 09:44:39 +0000

    ci(semantic release): attempt to parse inputs as json strings

--- analyze-commits.sh
+++ analyze-commits.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
 echo "commits" 1>&2
-
-for i in $1; do
-    echo "$i" 1>&2
+echo "$1" | jq -c '.[]' | while read -r commit; do
+    echo "$commit" 1>&2
 done
 
 echo "releases" 1>&2
-
-for i in $2; do
-    echo "$i" 1>&2
+echo "$2" | jq -c '.[]' | while read -r release; do
+    echo "$release" 1>&2
 done
 
 echo "lastRelease" 1>&2
-
-echo "$3" 1>&2
+echo "$3" | jq -c '.'
 
 if [ -n "$1" ]; then
     echo "A release is happening" 1>&2
-    echo "$1" 1>&2
     exit 1
 else
     echo "No release, killing CI" 1>&2
