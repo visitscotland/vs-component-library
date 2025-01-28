4bb56eef2a1dd6a1e5e1dd3f4bff17d04f9d4457
GemmaLouise <gemma_stephen@hotmail.co.uk>
2025-01-28 10:20:45 +0000

    feat(blogdetails): update blog details to allow optional author and date

--- src/components/blog-details/BlogDetails.vue
+++ src/components/blog-details/BlogDetails.vue
@@ -3,19 +3,36 @@
         class="vs-blog-details"
         data-test="vs-blog-details"
     >
-        <span class="vs-blog-details__author">
-            {{ blogAuthor }} <span class="vs-blog-details__divider">·</span>
-            <span class="vs-blog-details__date">
+        <div
+            v-if="blogAuthor"
+            class="vs-blog-details__author vs-blog-details--highlight"
+        >
+            {{ blogAuthor }}
+        </div>
+
+        <div
+            data-test="vs-blog-details__date-time"
+            :class="!blogAuthor ? 'vs-blog-details--highlight' : ''"
+            class="mb-150"
+        >
+            <VsIcon
+                name="clock"
+                :variant="iconVariant"
+                class="align-text-top me-050"
+            />{{ blogReadTime }}
+            <span
+                v-if="blogPublishDate"
+                class="vs-blog-details__date"
+            >
+                <span class="vs-blog-details__divider">•</span>
                 {{ blogPublishDate }}
             </span>
-        </span>
-        <span class="vs-blog-details__read-time">
-            {{ blogReadTime }}
-        </span>
+        </div>
     </div>
 </template>
 
 <script>
+import VsIcon from '@/components/icon/Icon.vue';
 
 /**
  * Displays relevant blog details such as author, date published
@@ -28,20 +45,23 @@ export default {
     name: 'VsBlogDetails',
     status: 'prototype',
     release: '0.0.1',
+    components: {
+        VsIcon,
+    },
     props: {
         /**
          * Name of the blog author
          */
         blogAuthor: {
             type: String,
-            required: true,
+            default: null,
         },
         /**
          * Date the blog was published
          */
         blogPublishDate: {
             type: String,
-            required: true,
+            default: null,
         },
         /**
          * How long the blog takes to read
@@ -51,27 +71,35 @@ export default {
             required: true,
         },
     },
+    computed: {
+        iconVariant() {
+            return this.blogAuthor ? 'secondary' : 'tertiary';
+        },
+    },
 };
 </script>
 
 <style lang="scss">
 .vs-blog-details {
-    &__author{
-        display: block;
-    }
-
     &__date {
-        color: $vs-color-text-tertiary;
-    }
-
-    &__read-time {
         display: block;
-        margin-bottom: $spacer-150;
-        color: $vs-color-text-tertiary;
+
+        @include media-breakpoint-up(sm) {
+            display: inline;
+        }
     }
 
     &__divider {
-        padding: 0 $spacer-050;
+        display: none;
+        padding: 0 $spacer-025;
+
+        @include media-breakpoint-up(sm) {
+            display: inline;
+        }
+    }
+
+    &--highlight {
+        color: $vs-color-text-tertiary;
     }
 }
 </style>
