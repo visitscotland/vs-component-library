fa569e1f7415b47dd356773611807e8faeb23277
Duncan Sanderson <duncan.sanderson@visitscotland.com>
2025-01-30 16:40:09 +0000

    feat(pagination): rework component

--- src/components/pagination/VsPagination.vue
+++ src/components/pagination/VsPagination.vue
@@ -1,67 +1,75 @@
 <template>
-    <div class="vs-pagination">
+    <nav
+        class="vs-pagination"
+        data-test="vs-pagination"
+        aria-label="Pagination"
+    >
         <div class="vs-pagination__count">
-            <p>Page: {{ currentPage }} of {{ totalPages }}</p>
+            <p>{{ resultsText }}</p>
         </div>
-        <BPagination
-            align="center"
-            :aria-controls="props.ariaControls"
-            class="vs-pagination__controls"
-            first-number
-            last-number
-            limit="5"
-            :per-page="props.itemsPerPage"
-            pills
-            :total-rows="props.numberOfItems"
-            v-model="currentPage"
-            @page-click="(event, page) => $emit('pageClick', page)"
-        >
-            <template #prev-text>
-                <VsIcon
-                    name="internal-link-back"
-                    size="xs"
-                />
-                {{ props.previousButtonLabel }}
-            </template>
+        <div class="vs-pagination__controls">
+            <div class="vs-pagination__prev">
+                <VsButton
+                    :disabled="currentPage === 1"
+                    icon="internal-link-back"
+                    size="sm"
+                    variant="transparent"
+                    @click="handleClick($event, currentPage - 1)"
+                >
+                    {{ props.previousButtonLabel }}
+                </VsButton>
+            </div>
 
-            <template #next-text>
-                {{ props.nextButtonLabel }}
-                <VsIcon
-                    name="internal-link"
-                    size="xs"
-                />
-            </template>
-        </BPagination>
-    </div>
+            <ul class="vs-pagination__list">
+                <li
+                    v-for="(page, index) in pages"
+                    class="vs-pagination__item"
+                    :class="{ 'vs-pagination__item--active': currentPage === page }"
+                    :key="index"
+                >
+                    <span
+                        v-if="page === 'ellipses'"
+                        class="vs-pagination__item--ellipses"
+                    >
+                        &ctdot;
+                    </span>
+                    <VsButton
+                        v-else
+                        :aria-current="currentPage === page ? 'page' : null"
+                        :aria-label="`Page ${page}`"
+                        size="sm"
+                        @click="handleClick($event, page)"
+                    >
+                        {{ page }}
+                    </VsButton>
+                </li>
+            </ul>
+
+            <div class="vs-pagination__next">
+                <VsButton
+                    :disabled="currentPage === props.numberOfPages"
+                    icon="internal-link"
+                    icon-position="right"
+                    size="sm"
+                    variant="transparent"
+                    @click="handleClick($event, currentPage + 1)"
+                >
+                    {{ props.nextButtonLabel }}
+                </VsButton>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script setup>
-/**
- * TODO: style buttons
- * TODO: mobile version
- * TODO: Add tests
- */
-
-import { computed, ref } from 'vue';
-import { BPagination } from 'bootstrap-vue-next';
-import VsIcon from '@/components/icon/Icon.vue';
+import {
+    computed,
+    ref,
+    watch,
+} from 'vue';
+import VsButton from '@/components/button/Button.vue';
 
 const props = defineProps({
-    /**
-     * ID of element being controlled by this pagination component.
-     */
-    ariaControls: {
-        type: String,
-        default: undefined,
-    },
-    /**
-     * The number of items displayed per page.
-     * Used to calculate number of pages.
-    */
-    itemsPerPage: {
-        type: Number,
-        required: true,
-    },
     /**
      * Label for the next button.
      */
@@ -70,11 +78,24 @@ const props = defineProps({
         required: true,
     },
     /**
-     * The total number of items.
-     * Used to calculate number of pages.
-    */
-    numberOfItems: {
+     * Total number of pages
+     */
+    numberOfPages: {
         type: Number,
+        required: true,
+    },
+    /**
+     * Label for "Page" within "Page x of xx" text.
+     */
+    pageLabel: {
+        type: String,
+        required: true,
+    },
+    /**
+     * Label for "of" within "Page x of xx" text.
+     */
+    ofLabel: {
+        type: String,
         required: true,
     },
     /**
@@ -86,70 +107,213 @@ const props = defineProps({
     },
 });
 
-defineEmits(['pageClick']);
+const emit = defineEmits(['page-click']);
 
 // Track current page number.
 const currentPage = ref(1);
 
-// Calculate number of pages.
-const totalPages = computed(() => Math.ceil(props.numberOfItems / props.itemsPerPage));
+// Number of page buttons to display.
+const limit = 7;
+
+// Format text shown above controls on mobile.
+const resultsText = computed(() => `${props.pageLabel} ${currentPage.value} ${props.ofLabel} ${props.numberOfPages}`);
+
+// Create list of page buttons.
+const pages = computed(() => {
+    // If there are fewer pr equal pages than the limit, show all pages.
+    if (props.numberOfPages <= limit) {
+        // Create an array of numbers from 1 to the number of pages.
+        return Array.from({
+            length: props.numberOfPages,
+        }, (_, i) => i + 1);
+    }
+
+    let buttons = [];
+    const halfLimit = Math.floor(limit / 2);
+
+    // If current page is within the first three pages then show the first
+    // five pages, an ellipsis and the last page.
+    if (currentPage.value <= halfLimit) {
+        for (let index = 1; index < limit - 1; index++) {
+            buttons = [...buttons, index];
+        }
+
+        buttons = [...buttons, 'ellipses', props.numberOfPages];
+
+    // If current page is within the last three pages then show the first
+    // pages, an ellipsis and the last five pages.
+    } else if (currentPage.value > props.numberOfPages - halfLimit) {
+        buttons = [1, 'ellipses'];
+
+        const start = props.numberOfPages - limit + 2;
+        for (let index = 1; index < limit - 1; index++) {
+            buttons = [...buttons, start + index];
+        }
+
+    // If current page is not any of the above then show the first
+    // page, an ellipsis, three middle pages, an ellipsis and the last pages.
+    } else {
+        const start = (currentPage.value - halfLimit) + 1;
+        buttons = [1, 'ellipses'];
+
+        for (let index = 1; index < limit - 3; index++) {
+            buttons = [...buttons, start + index];
+        }
+
+        buttons = [...buttons, 'ellipses', props.numberOfPages];
+    }
+
+    return buttons;
+});
+
+// Emit event when a page button is clicked and update the current page.
+const handleClick = (event, page) => {
+    if (currentPage.value === page) return;
+
+    emit('page-click', page);
+    currentPage.value = page;
+};
+
+// Reset current page to 1 when number of pages changes.
+watch(() => props.numberOfPages, () => {
+    currentPage.value = 1;
+});
 </script>
 
 <style lang="scss">
 .vs-pagination {
-    &__controls {
-        .page-item {
-            &:not(.disabled) {
-                .page-link {
-                    @include vs-button-variant(
-                        $vs-color-text-primary, $vs-color-background-inverse, $vs-color-border-inverse,
-                        $vs-color-text-primary, $vs-color-background-inverse, $vs-color-border-inverse,
-                        $vs-color-text-inverse, $vs-color-interaction-cta-primary, $vs-color-border-inverse,
-                    );
-
-                    &:hover {
-                        text-decoration: underline;
-                    }
-                }
-
-                &:first-child,
-                &:last-child {
-                    .page-link,
-                    .page-link .vs-icon  {
-                        color: $vs-color-interaction-link-primary;
-                    }
-                }
-
-                &.active .page-link {
-                    background-color: $vs-color-interaction-cta-primary;
-                    color: $vs-color-text-inverse;
-                }
-            }
-
-            &.disabled .page-link,
-            &.disabled .vs-icon {
-                background-color: $vs-color-background-inverse;
-                border-color: $vs-color-border-inverse;
-                color: $vs-color-text-disabled;
-            }
-        }
-
-        @include media-breakpoint-down(md) {
-            justify-content: space-between !important;
-
-            .page-item:not(:first-child, :last-child) {
-                display: none;
-            }
-        }
-    }
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
     &__count {
-        text-align: center;
-        width: 100%;
+        display: flex;
 
         @include media-breakpoint-up(md) {
             display: none;
         }
     }
+
+    &__controls {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+
+        @include media-breakpoint-down(md) {
+            justify-content: space-between;
+        }
+    }
+
+    &__list {
+        display: flex;
+        flex-direction: row;
+        list-style: none;
+        padding-left: $spacer-0;
+
+        @include media-breakpoint-down(md) {
+            display: none;
+        }
+    }
+
+    &__item {
+        padding: 0 $spacer-025;
+
+        .vs-button {
+            @include vs-button-variant(
+                $vs-color-text-primary, $vs-color-background-inverse, $vs-color-border-inverse,
+                $vs-color-text-primary, $vs-color-background-inverse, $vs-color-border-inverse,
+                $vs-color-text-inverse, $vs-color-interaction-cta-primary, $vs-color-border-inverse,
+            );
+            padding: $spacer-0125 $spacer-075;
+
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+
+        .vs-button.disabled {
+            background-color: $vs-color-background-inverse !important;
+        }
+
+        &--active .vs-button {
+            background-color: $vs-color-interaction-cta-primary;
+            color: $vs-color-text-inverse;
+        }
+
+        &--ellipses {
+            vertical-align: sub;
+        }
+    }
+
+    &__prev .vs-button,
+    &__next .vs-button {
+        border: none;
+        padding-top: $spacer-025;
+
+        &.disabled {
+            background-color: $vs-color-background-inverse !important;
+        }
+
+        &:hover {
+            background-color: $vs-color-background-inverse !important;
+            text-decoration: underline;
+        }
+    }
 }
+// .vs-pagination {
+//     &__controls {
+//         .page-item {
+//             &:not(.disabled) {
+//                 .page-link {
+//                     @include vs-button-variant(
+//                         $vs-color-text-primary, $vs-color-background-inverse, $vs-color-border-inverse,
+//                         $vs-color-text-primary, $vs-color-background-inverse, $vs-color-border-inverse,
+//                         $vs-color-text-inverse, $vs-color-interaction-cta-primary, $vs-color-border-inverse,
+//                     );
+
+//                     &:hover {
+//                         text-decoration: underline;
+//                     }
+//                 }
+
+//                 &:first-child,
+//                 &:last-child {
+//                     .page-link,
+//                     .page-link .vs-icon  {
+//                         color: $vs-color-interaction-link-primary;
+//                     }
+//                 }
+
+//                 &.active .page-link {
+//                     background-color: $vs-color-interaction-cta-primary;
+//                     color: $vs-color-text-inverse;
+//                 }
+//             }
+
+//             &.disabled .page-link,
+//             &.disabled .vs-icon {
+//                 background-color: $vs-color-background-inverse;
+//                 border-color: $vs-color-border-inverse;
+//                 color: $vs-color-text-disabled;
+//             }
+//         }
+
+//         @include media-breakpoint-down(md) {
+//             justify-content: space-between !important;
+
+//             .page-item:not(:first-child, :last-child) {
+//                 display: none;
+//             }
+//         }
+//     }
+
+//     &__count {
+//         text-align: center;
+//         width: 100%;
+
+//         @include media-breakpoint-up(md) {
+//             display: none;
+//         }
+//     }
+// }
 </style>
