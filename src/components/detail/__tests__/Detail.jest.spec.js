100b03c40478140c881cb113bd9746ea637a8997
GemmaLouise <gemma_stephen@hotmail.co.uk>
2025-06-27 16:04:45 +0100

    test(detail): add unit tests for new detail component

--- src/components/detail/__tests__/Detail.jest.spec.js
+++ src/components/detail/__tests__/Detail.jest.spec.js
@@ -1,5 +1,5 @@
 import { shallowMount } from '@vue/test-utils';
-
+import axe from '@/../test/unit/helpers/axe-helper';
 import VsDetail from '../Detail.vue';
 
 const slotText = 'Detail content';
@@ -17,7 +17,33 @@ describe('VsDetail', () => {
     it('should render a div with the class `vs-detail`', () => {
         const wrapper = factoryShallowMount();
 
-        expect(wrapper.element.tagName).toBe('DIV');
+        expect(wrapper.element.tagName).toBe('P');
         expect(wrapper.classes('vs-detail')).toBe(true);
+        expect(wrapper.classes()).toContain('vs-detail--medium');
+    });
+
+    describe(':props', () => {
+        it(':size - should accept and render a `size` prop', () => {
+            const wrapper = factoryShallowMount({
+                size: 'large',
+            });
+
+            expect(wrapper.classes()).toContain('vs-detail--large');
+        });
+    });
+
+    describe('slots:', () => {
+        it('should render content inserted into `default` slot', () => {
+            const wrapper = factoryShallowMount();
+
+            expect(wrapper.text()).toContain(slotText);
+        });
+    });
+
+    describe(':accessibility', () => {
+        it('should not have aXe accessibility issues', async() => {
+            const wrapper = factoryShallowMount();
+            expect(await axe(wrapper.html())).toHaveNoViolations();
+        });
     });
 });
