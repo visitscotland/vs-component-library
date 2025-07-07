e3dfec3574279527bd99737dd6a542615ea213f3
GemmaLouise <gemma_stephen@hotmail.co.uk>
2025-07-07 16:19:21 +0100

    refactor(tokens): update font-size tokens to use vs-

--- src/components/canned-search/components/CannedSearchSubHeading.vue
+++ src/components/canned-search/components/CannedSearchSubHeading.vue
@@ -59,7 +59,7 @@ export default {
         margin-bottom: $spacer-075;
 
         @include media-breakpoint-up(md) {
-            font-size: $heading-xxs;
+            font-size: $vs-font-size-heading-xl;
         }
 
         &--lines-1 {
