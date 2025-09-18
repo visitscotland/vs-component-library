404c27bfaec5a25f47740c52b5e3628e0158a7a8
mprosser <Michael.Prosser@visitscotland.com>
2025-09-18 10:51:15 +0100

    fix(civic): remove magic numbers

--- src/utils/required-cookies-data.js
+++ src/utils/required-cookies-data.js
@@ -1,11 +1,16 @@
-/**
- * 0 = Analytical
- * 1 = Marketing
- */
+export const cookieCategories = Object.freeze({
+    analytical: 0,
+    marketing: 1,
+});
 
 const cookieValues = {
-    youtube: [1],
-    embed: [0, 1],
+    youtube: [
+        cookieCategories.marketing,
+    ],
+    embed: [
+        cookieCategories.analytical,
+        cookieCategories.marketing,
+    ],
 };
 
 export default cookieValues;
