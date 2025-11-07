0666d3fb6f59d901fefc01d6a45af07aea51efbb
chesterroberts-vs <chester.roberts@visitscotland.com>
2025-11-07 16:15:26 +0000

    feat(verifycookiescomposable): adds composable version of verifyCookiesMixin to work with VsMainMap

---
+++ src/custom-components/maps/composables/verifyCookiesComposable.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-undef */
+
+import {
+    computed,
+    onMounted,
+    ref,
+} from 'vue';
+
+const cookieCheckerComposable = () => {
+    const requiredCookies = ref([]);
+    const requiredCookiesAllowed = ref(false);
+    const cookieManagerLoaded = ref(false);
+
+    const cookiesAllowed = computed(() => {
+        if (typeof window !== 'undefined' && window.bypassCookiesRequired) {
+            return true;
+        };
+
+        return {
+            requiredCookiesAllowed,
+        };
+    });
+
+    const cookiesLoaded = computed(() => {
+        if (typeof window !== 'undefined' && window.bypassCookiesLoaded) {
+            return true;
+        }
+
+        return {
+            cookieManagerLoaded,
+        };
+    });
+
+    function managerLoaded() {
+        cookieManagerLoaded.value = true;
+    }
+
+    function cookiesUpdated() {
+        let allowed = false;
+
+        if (typeof CookieControl !== 'undefined') {
+            allowed = requiredCookies.value.every(
+                (category) => CookieControl
+                    && CookieControl.getCategoryConsent(category) !== false,
+            );
+        };
+
+        requiredCookiesAllowed.value = allowed;
+
+        if (typeof callbackOnUpdated !== 'undefined') {
+            callbackOnUpdated();
+        }
+    }
+
+    onMounted(() => {
+        window.addEventListener('cookieManagerLoaded', () => {
+            managerLoaded();
+        });
+
+        window.addEventListener('cookiesUpdated', () => {
+            cookiesUpdated();
+        });
+    });
+
+    return {
+        cookiesAllowed,
+        cookiesLoaded,
+    };
+};
+
+export default cookieCheckerComposable;
