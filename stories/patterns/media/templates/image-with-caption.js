867d36a0c7460b8c84b1f9690a11451f38b1204e
GemmaLouise <gemma_stephen@hotmail.co.uk>
2025-12-12 13:46:08 +0000

    docs(image caption): update image caption component to use <figure>

--- stories/patterns/media/templates/image-with-caption.js
+++ stories/patterns/media/templates/image-with-caption.js
@@ -1,14 +1,18 @@
 export default `
+<figure>
     <VsImg 
         src="./fixtures/image-with-caption/images/blair-castle.webp"
         fluid
     />
-    <VsMediaCaption>
-        <template v-slot:caption>
-            Blair Castle, Blair Atholl, Perthshire
-        </template>
-        <template v-slot:credit>
-            © VisitScotland / Paul Tomkins
-        </template>
-    </VsMediaCaption>
+    <figcaption>
+        <VsMediaCaption>
+            <template v-slot:caption>
+                Blair Castle, Blair Atholl, Perthshire
+            </template>
+            <template v-slot:credit>
+                © VisitScotland / Paul Tomkins
+            </template>
+        </VsMediaCaption>
+    </figcaption>
+</figure>
 `;
