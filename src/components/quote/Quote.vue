67062bf0d562aa0b52b109ab2eae3f58b8899306
GemmaLouise <gemma_stephen@hotmail.co.uk>
2026-05-15 17:28:04 +0100

    fix(quotw): fix broken blockquote

--- src/components/quote/Quote.vue
+++ src/components/quote/Quote.vue
@@ -123,6 +123,16 @@ export default {
     },
     props: {
         /**
+         * Sets HTML tag for correct quote type (e.g. blockquote for
+         * long quotes, q for short quotes, aside for pull quotes,
+         * div for generic quotes)
+         */
+        type: {
+            type: String,
+            default: 'blockquote',
+            validator: (value) => value.match(/(blockquote|aside|q|div)/),
+        },
+        /**
          * Sets the quote text content.
          */
         quoteText: {
--- stories/components/Quote.stories.js
+++ stories/components/Quote.stories.js
@@ -36,6 +36,8 @@ const Template = (args) => ({
 const blockquoteText = 'Afterwards, if you are ready for dinner, you could catch the tram to the Leith area of the city. It\'s one of the trendiest neighbourhoods in the UK and boasts three Michelin-star restaurants. Alternatively, wander along Princes Street or George Street to the city\'s West End.';
 const pullquoteText = 'It\'s one of the trendiest neighbourhoods in the UK';
 const base = {
+    useLegacy: false,
+    type: 'blockquote',
     quoteText: blockquoteText,
 };
 
