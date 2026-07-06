c509f834b44923b43bba3532e0c6f8566db93501
GemmaLouise <gemma_stephen@hotmail.co.uk>
2026-07-06 16:27:16 +0100

    chore(babek): removes some unused babel packages

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,24 @@
+/**
+ * Babel config used only by Jest (via babel-jest).
+ *
+ * Purpose:
+ * - Transpiles modern JS so Jest can execute it in a Node environment
+ * - Only applies during unit tests (not used by Vite or production builds)
+ *
+ * - Required for current Jest + Vue SFC test setup
+ * - Vue files are handled separately by @vue/vue3-jest
+ *
+ * Can be removed when:
+ * - Migrating tests to Vitest, or
+ * - Removing Jest / babel-jest from the project
+ */
+
 module.exports = {
     presets: ['@babel/preset-env'],
-    plugins: [
-        '@babel/plugin-transform-runtime',
-        'macros',
-    ],
-    exclude: [],
     ignore: [/node_modules\/(?!bootstrap-vue-next)/],
     env: {
         test: {
             presets: ['@babel/preset-env'],
-            plugins: [
-                'macros',
-            ],
         },
     },
 };
