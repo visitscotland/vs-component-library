918dacda54e537a3b66d15179e99591911ba15b2
Duncan Sanderson <duncan.sanderson@visitscotland.com>
2026-07-07 12:22:30 +0100

    fix(useupdatesearchparams.ts): correct types and remove unused code

--- src/custom-components/maps/composables/useUpdateSearchParams.ts
+++ src/custom-components/maps/composables/useUpdateSearchParams.ts
@@ -1,12 +1,12 @@
 export default function useUpdateSearchParams() {
-    function updateSearchParams(updates: Record<string, string>) {
+    function updateSearchParams(updates: Record<string, string | null>) {
         const url = new URL(window.location.href);
         
         for (const [key, value] of Object.entries(updates)) {
             if (value === null || value === '') {
                 url.searchParams.delete(key);
             } else {
-                url.searchParams.set(key, encodeURI(value));
+                url.searchParams.set(key, value);
             }
         }
         
@@ -17,21 +17,12 @@ export default function useUpdateSearchParams() {
     function getSearchParams() {
         const params = new URLSearchParams(window.location.search);
         const category = params.get('category');
-        const coords = params.get('coords');
         const location = params.get('location');
-        const subcategories = params.get('subcategories');
         
-        let searchTerm = params.get('search-term');
-        if (searchTerm) {
-            searchTerm = decodeURI(searchTerm);
-        }
 
         return {
             category,
-            coords,
             location,
-            searchTerm,
-            subcategories,
         };
     }
 
