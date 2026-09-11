2e23dae0e19a37183683627e4981c40f600d09c6
mjpvs <Michael.Prosser@visitscotland.com>
2026-09-11 14:43:34 +0100

    fix(forms): remove redundant story

--- stories/components/Form.stories.js
+++ stories/components/Form.stories.js
@@ -93,15 +93,6 @@ NoRecaptcha.args = {
     usesRecaptcha: false,
 };
 
-export const RequiredCheckboxes = Template.bind({
-});
-
-RequiredCheckboxes.args = {
-    ...base,
-    usesRecaptcha: false,
-    dataUrl: './fixtures/forms/required-checkboxes-form.json',
-};
-
 export const HiddenFields = Template.bind({
 });
 
